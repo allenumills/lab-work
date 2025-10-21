716c9f0ae113a7ccb04f9365060ae1bc78bc01c6
Allen U Mills <allenumills@gmail.com>
2025-10-21 06:45:53 -0700

    finished the lap work

--- week3_1.py
+++ week3_1.py
@@ -15,4 +15,35 @@ cost = [15.00 , 12.50 , 3.75, 40.25, 6,99]
 total_cost = 0
 for costes in cost:
     total_cost += costes
-print(f"Total cost: $, {total_cost:.2f}") 
+print(f"Total cost: $, {total_cost:.2f}")
+
+# Exercise 3: Nassted for loops - pattern generation
+for row in range(5):
+    for column in range(5):
+        print("*", end=" ")
+    print()  # Move to the next line after each row
+
+# step 2: create a right- angled triangle pattern
+for row in range(1, 6):
+    for column in range(row):
+        print("*", end=" ")
+    print()  # Move to the next line after each row
+
+# Exercise 4: While loops - menu- driver calculator
+while choice != 'Q':
+    print("******Menu******")
+    print("1. Add")
+    print("2. Subtract")
+    print("Q. Quit")
+    choice = input("Please select an option: ")
+
+    if choice == '1':
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        result = num1 + num2
+        print(f"The result is: {result}")
+    elif choice == '2':
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        result = num1 - num2
+        print(f"The result is: {result}")
